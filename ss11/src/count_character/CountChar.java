1422880f95d5c6038f5954ac191b1435a90272e8
buitanhau <hau48912@gmail.com>
2023-06-07 19:26:50 +0700

    update bai tap 07062023

--- ss11/src/count_character/CountChar.java
+++ ss11/src/count_character/CountChar.java
@@ -3,14 +3,27 @@ package count_character;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class CountChar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter str : ");
+        System.out.println("enter str with space : ");
         String str = scanner.nextLine();
-
+        Map<String, Integer> wordsCountMap = new HashMap<>();
+        String[] words = str.split("\\s+");
+        // \\s+ khi nhap khoang trang thi count se k tang
+        for (String word : words) {
+            if (wordsCountMap.containsKey(word)) {
+                int count = wordsCountMap.get(word);
+                wordsCountMap.put(word, count + 1);
+            } else {
+                wordsCountMap.put(word, 1);
+            }
+        }
+        System.out.println("So lan xuat hien cua tung tu trong chuoi la :");
+        for (String word : wordsCountMap.keySet()) {
+            System.out.println(word + " : " + wordsCountMap.get(word));
+        }
     }
 
 }
